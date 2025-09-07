2cc4f97ad85bdf2dec6cf115d967298298ab11e8
LeckerSchmecker123 <lmuth@willigis-online.de>
2025-09-07 18:29:55 +0200

    Alle Aufgaben gemacht Blatt 03, 7.9.

--- Schule/src/blatt03/MitOderOhneStrom.java
+++ Schule/src/blatt03/MitOderOhneStrom.java
@@ -18,6 +18,7 @@ public class MitOderOhneStrom {
             System.out.println("Befindet sich auf dem Eingang Strom? (true/false)");
             boolean eingang = input.nextBoolean();
 
+            // Fallunterscheidung
             if (eingang == true) { // Fall 1.1: Es ist Strom auf dem Eingang, dadurch ist beim Ausgang kein Strom.
                 System.out.println("NOT " + eingang + " = false");
             } else if (eingang == false) { // Fall 1.2: Es ist kein Strom auf dem Eingang, dadurch ist beim Ausgang Strom.
@@ -34,6 +35,7 @@ public class MitOderOhneStrom {
             System.out.println("Befindet sich auf dem zweiten Eingang Strom? (true/false)");
             boolean eingang2 = input.nextBoolean();
 
+            // Fallunterscheidung
             if (eingang1 == true && eingang2 == true) { // Fall 2.1: Auf beiden Eingängen befindet sich Strom, dadurch befindet sich auf dem Ausgang Strom.
                 System.out.println(eingang1 + " OR " + eingang2 + " = true");
             } else if (eingang1 == true && eingang2 == false) { // Fall 2.2: Nur auf dem ersten Eingang befindet sich Strom, dadurch befindet sich auf dem Ausgang Strom.
@@ -48,9 +50,43 @@ public class MitOderOhneStrom {
             System.out.println("Das AND-Gatter:");
             System.out.println("Das Gatter verfügt über 2 Eingänge und einen Ausgang.\n");
 
+            // Fragen, ob sich Strom auf den beiden Ausgängen befindet
+            System.out.println("Befindet sich auf dem ersten Eingang Strom? (true/false)");
+            boolean eingang1 = input.nextBoolean();
+            System.out.println("Befindet sich auf dem zweiten Eingang Strom? (true/false)");
+            boolean eingang2 = input.nextBoolean();
+
+            //Fall Unterscheidung
+            if (eingang1 == true && eingang2 == true) { // Fall 3.1: Auf beiden Eingängen befindet sich Strom, dadurch befindet sich auf dem Ausgang Strom.
+                System.out.println(eingang1 + " AND " + eingang2 + " = true");
+            } else if (eingang1 == true && eingang2 == false) { // Fall 3.2: Nur auf dem ersten Eingang befindet sich Strom, dadurch befindet sich auf dem Ausgang kein Strom.
+                System.out.println(eingang1 + " AND " + eingang2 + " = false");
+            } else if (eingang1 == false && eingang2 == true) { // Fall 3.3: Nur auf dem zweiten Eingang befindet sich Strom, dadurch befindet sich auf dem Ausgang kein Strom.
+                System.out.println(eingang1 + " AND " + eingang2 + " = false");
+            } else if (eingang1 == false && eingang2 == false) { // Fall 3.4: Auf keinen Eingang befindet sich Strom, dadurch befindet sich auf dem Ausgang kein Strom.
+                System.out.println(eingang1 + " AND " + eingang2 + " = false");
+            }
+
         } else if (antwortGatter.equals("XOR")) { // Fall 4: Nutzer hat sich für das XOR-Gatter entschieden.
             System.out.println("Das XOR-Gatter:");
             System.out.println("Das Gatter verfügt über 2 Eingänge und einen Ausgang.\n");
+
+            // Fragen, ob sich Strom auf den beiden Ausgängen befindet
+            System.out.println("Befindet sich auf dem ersten Eingang Strom? (true/false)");
+            boolean eingang1 = input.nextBoolean();
+            System.out.println("Befindet sich auf dem zweiten Eingang Strom? (true/false)");
+            boolean eingang2 = input.nextBoolean();
+
+            // Fallunterscheidung
+            if (eingang1 == true && eingang2 == true) { // Fall 4.1: Auf beiden Eingängen befindet sich Strom, dadurch befindet sich auf dem Ausgang kein Strom.
+                System.out.println(eingang1 + " XOR " + eingang2 + " = false");
+            } else if (eingang1 == true && eingang2 == false) { // Fall 4.2: Nur auf dem ersten Eingang befindet sich Strom, dadurch befindet sich auf dem Ausgang Strom.
+                System.out.println(eingang1 + " XOR " + eingang2 + " = true");
+            } else if (eingang1 == false && eingang2 == true) { // Fall 4.3: Nur auf dem zweiten Eingang befindet sich Strom, dadurch befindet sich auf dem Ausgang Strom.
+                System.out.println(eingang1 + " XOR " + eingang2 + " = true");
+            }  else if (eingang1 == false && eingang2 == false) { // Fall 4.4: Auf keinen der beiden Eingängen befindet sich Strom, dadurch befindet sich auf dem Ausgang kein Strom.
+                System.out.println(eingang1 + " XOR " + eingang2 + " = false");
+            }
 
         } else { // Fehlerfall: Nutzer hat eine invalide Antwort gegeben.
             System.out.println("Dies ist eine falsche Eingabe");
