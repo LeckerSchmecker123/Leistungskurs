112c72fad6ed66731b7c8c5bc56d7bbce3c5aac7
LeckerSchmecker123 <lmuth@willigis-online.de>
2025-09-07 17:58:37 +0200

    NOT Gatter fertig

--- Schule/src/blatt03/MitOderOhneStrom.java
+++ Schule/src/blatt03/MitOderOhneStrom.java
@@ -12,21 +12,31 @@ public class MitOderOhneStrom {
 
         if (antwortGatter.equals("NOT")) { // Fall 1: Nutzer hat sich für das NOT-Gatter entschieden.
             System.out.println("Das NOT-Gatter:");
-            System.out.println("Das Gatter verfügt über einen Eingang und einen Ausgang.");
+            System.out.println("Das Gatter verfügt über einen Eingang und einen Ausgang.\n");
+
+            System.out.println("Befindet sich auf dem Eingang Strom? (true/false)");
+            boolean eingang = input.nextBoolean();
+
+            if (eingang == true) {
+                System.out.println(eingang + " = false");
+            } else if (eingang == false) {
+                System.out.println(eingang + " = true");
+            }
 
         } else if (antwortGatter.equals("OR")) { // Fall 2: Nutzer hat sich für das OR-Gatter entschieden.
             System.out.println("Das OR-Gatter:");
-            System.out.println("Das Gatter verfügt über mindestens 2 Eingänge und einen Ausgang.");
+            System.out.println("Das Gatter verfügt über mindestens 2 Eingänge und einen Ausgang.\n");
+
 
         } else if (antwortGatter.equals("AND")) { // Fall 3: Nutzer hat sich für das AND-Gatter entschieden.
             System.out.println("Das AND-Gatter:");
-            System.out.println("Das Gatter verfügt über 2 Eingänge und einen Ausgang.");
+            System.out.println("Das Gatter verfügt über 2 Eingänge und einen Ausgang.\n");
 
         } else if (antwortGatter.equals("XOR")) { // Fall 4: Nutzer hat sich für das XOR-Gatter entschieden.
             System.out.println("Das XOR-Gatter:");
-            System.out.println("Das Gatter verfügt über 2 Eingänge und einen Ausgang.");
+            System.out.println("Das Gatter verfügt über 2 Eingänge und einen Ausgang.\n");
 
-        } else { // Ausnahme Fall: Nutzer hat eine invalide Antwort gegeben.
+        } else { // Fehlerfall: Nutzer hat eine invalide Antwort gegeben.
             System.out.println("Dies ist eine falsche Eingabe");
             System.exit(0);
         }
