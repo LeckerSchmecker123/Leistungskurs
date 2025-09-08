03cc8ba2b16d8962015c0df1d9bdb6da5dc02b3b
LeckerSchmecker123 <lmuth@willigis-online.de>
2025-09-08 09:50:47 +0200

    blatt 04, while schleife fertig gemacht

--- Schule/src/blatt04/ETF.java
+++ Schule/src/blatt04/ETF.java
@@ -4,14 +4,20 @@ public class ETF {
     public static void main(String[] args) {
 
         int monat = 0;
-        double sparbetrag = 50;
-        double gesamtbetrag = 0;
+        double sparbetrag = 50; //€
+        double gesamtbetrag = 0; //€
+        double anteilETF = 3578.89; //€
+        double anteil = 0; //%
 
         while (monat < 12) {
             monat++;
+            gesamtbetrag += sparbetrag;
+            anteil = gesamtbetrag / anteilETF;
+
             System.out.println("Monat: " + monat);
             System.out.printf("Sparbetrag: %.2f € %n", sparbetrag);
-            System.out.printf("Gesamtbetrag: %.2f € %n%n", gesamtbetrag + sparbetrag);
+            System.out.printf("Gesamtbetrag: %.2f € %n", gesamtbetrag);
+            System.out.printf("Anteil: %.5f %%%n%n",  anteil);
 
         }
     }
