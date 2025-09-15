15e9bc93909fa0db36e3b09b41ae8b7c69d1830d
LeckerSchmecker123 <lmuth@willigis-online.de>
2025-09-15 12:49:55 +0200

    von der Stunde kegeln weitergemacht aber noch nicht fertig (Punkteberechnung)

--- Schule/src/blatt04/Struktogramm.java
+++ Schule/src/blatt04/Struktogramm.java
@@ -1,5 +1,63 @@
 package blatt04;
 
-public class Struktogramm {
+import java.util.Scanner;
 
+public class Struktogramm {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int maximalPunkte = 270;
+        int punkteTotal = 0;
+        int wurf1Punkte = 0;
+        int wurf2Punkte = 0;
+        int runde = 0;
+        int multiplyer = 1;
+        int wurf = 0;
+
+        while (true) {
+            runde ++;
+            System.out.println("Runde: " + runde);
+            System.out.println("Wie viele Pins hast du beim ersten Wurf getroffen?");
+            wurf1Punkte = input.nextInt();
+            if (wurf1Punkte < 0 || wurf1Punkte > 10) {
+                continue;
+            }
+            wurf ++;
+            if (wurf1Punkte == 10 && wurf == 1) {
+                multiplyer ++;
+                punkteTotal = (wurf1Punkte + wurf2Punkte) * multiplyer;
+                System.out.println("Deine Punktzahl: " +  punkteTotal + "\n");
+                continue;
+            }
+            System.out.println("Wie viele Punkte hast du beim zweiten Wurf getroffen?");
+            wurf2Punkte = input.nextInt();
+            while (wurf2Punkte < 0 || wurf2Punkte > 10 - wurf1Punkte) {
+                System.out.println("Wie viele Punkte hast du beim zweiten Wurf?");
+                wurf2Punkte = input.nextInt();
+            }
+            wurf ++;
+            if (wurf1Punkte + wurf2Punkte == 10 && wurf == 2) {
+                multiplyer ++;
+                punkteTotal = (wurf1Punkte + wurf2Punkte) * multiplyer;
+                System.out.println("Deine Punktzahl: " + punkteTotal + "\n");
+                continue;
+            }
+            punkteTotal = wurf1Punkte + wurf2Punkte;
+            System.out.println("Deine Punktzahl: " + punkteTotal + "\n");
+            multiplyer = 1;
+            wurf = 0;
+
+            if (punkteTotal > maximalPunkte) {
+                System.out.println("Du hast du maximal Punktzahl überschritten");
+            } else if (punkteTotal == maximalPunkte) {
+                System.out.println("Gewonnen!");
+                break;
+            }
+
+            if (runde == 10) {
+                System.out.println("Das Spiel ist vorbei");
+                break;
+            }
+        }
+    }
 }
