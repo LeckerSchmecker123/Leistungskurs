a7f91c0b5946662e392d93cb610b4135ee6a5c0a
LeckerSchmecker123 <lmuth@willigis-online.de>
2025-09-26 17:13:12 +0200

    AB (Hausaufgabe) weitergemacht, hab kein bock mehr

--- Schule/src/blatt06/Wertetabelle.java
+++ Schule/src/blatt06/Wertetabelle.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 public class Wertetabelle {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        System.out.println("Welche Basis soll das Polynom haben?");
+        int x = input.nextInt();
+
         System.out.println("Welchen Grad soll das Polynom haben?");
         int n = input.nextInt();
 
@@ -12,8 +16,7 @@ public class Wertetabelle {
 
         for (int i = 0; i < koeff.length; i++) {
             System.out.println("Nenne den Koeffizient " + i + ":");
-            koeff[i] = input
-                    .nextDouble();
+            koeff[i] = input.nextDouble();
         }
 
         System.out.println("Gib die Grenzen der Wertetabelle ein.");
@@ -25,5 +28,13 @@ public class Wertetabelle {
             a = b;
             b = tmp;
         }
+
+        double f =0;
+
+        for (int i = 0; i < koeff.length; i++) {
+            f = koeff[i] * Math.pow(x, i);
+            System.out.println(f);
+        }
+
     }
 }
