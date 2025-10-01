c5eeac7ac3f62069b9129561cfa8c566bf13d2b6
LeckerSchmecker123 <lmuth@willigis-online.de>
2025-10-01 20:44:53 +0200

    Blatt 06 aufgabe 3 fertig

--- Schule/src/blatt08/SuchenUndErsetzen.java
+++ Schule/src/blatt08/SuchenUndErsetzen.java
@@ -1,13 +1,32 @@
 package blatt08;
 
+import java.util.Scanner;
+
 public class SuchenUndErsetzen {
 
-    // ersetzt alle a mit d TODO: allgemein machen und groß und klein schreibung beachten
-    public static String ersetzen (String str) {
+    /**
+     * Die Prozedur tauscht einen angegebenen Buchstaben durch einen anderen angegebenen Buchstaben aus, die Wörter, in denen der Tausch stattfindet, wird ebenfalls angeben
+     * @param str Wort(e)
+     * @param ersetzen Buchstabe der ersetzt werden soll
+     * @param ersatz Buchstabe der den alen Buchstabe ersetzten soll
+     * @return ausgabe der Wort(e) mit dem neuen Buchstaben
+     */
+    public static String ersetzen (String str, String ersetzen, String ersatz) {
+        char[] ersetzenKlein = ersetzen.toCharArray();
+        char[] ersatzKlein = ersatz.toCharArray();
+
+        ersetzen = ersetzen.toUpperCase();
+        ersatz = ersatz.toUpperCase();
+
+        char [] ersetzenGross = ersetzen.toCharArray();
+        char [] ersatzGross = ersatz.toCharArray();
+
         char [] charr = str.toCharArray();
         for  (int i = 0; i < charr.length; i++) {
-            if (charr[i] == 'a') {
-                charr[i] = 'd';
+            if (charr[i] == ersetzenKlein[0]) {
+                charr[i] = ersatzKlein[0];
+            } else if (charr[i] == ersetzenGross[0]) {
+                charr[i] = ersatzGross[0];
             }
         }
         String s = new String(charr);
@@ -15,6 +34,14 @@ public class SuchenUndErsetzen {
     }
 
     public static void main(String[] args) {
-        System.out.println(ersetzen("Alles was er an hat ist an"));
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Bitte geben Sie ein oder mehrere Wörter ein:");
+        String worte = input.nextLine();
+        System.out.println("Bitte geben Sie nun den Buchstaben ein der ersetzt werden soll:");
+        String ersetzen = input.nextLine();
+        System.out.println("Bitte geben Sie nun den Buchstaben, der den alten ersetzten soll");
+        String ersatz = input.nextLine();
+        System.out.println(ersetzen(worte, ersetzen, ersatz));
     }
 }
