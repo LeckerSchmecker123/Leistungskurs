34ec1759d7d163f6c65027d147d70a9096187c99
LeckerSchmecker123 <lmuth@willigis-online.de>
2025-10-18 14:27:51 +0200

    HA weitergemacht

--- Schule/src/blatt08/Kalender.java
+++ Schule/src/blatt08/Kalender.java
@@ -64,6 +64,58 @@ public class Kalender {
         } else return false;
     }
 
+    /**
+     *Die Prozedur baut aus den 3 angegebenen Parametern (Tag, Monat, Jahr) das Datum zusammen
+     * @param tag Ganzzahl 1
+     * @param monat Ganzzahl 2
+     * @param jahr Ganzzahl 3
+     * @return Ausgabe des Datums
+     */
+    public static String zuDatum (int tag, int monat, int jahr) {
+        String datum = "";
+
+        if (tag < 10) {
+            datum = tag + "0";
+        }
+        datum = datum + tag;
+        datum = datum + ".";
+
+        if (monat < 10) {
+            datum = datum + "0";
+        }
+        datum = datum + monat;
+        datum = datum + ".";
+
+        datum = datum + jahr;
+
+        return datum;
+    }
+
+    //TODO: Wenn 31. gibt es probleme
+    public static String naechsterTag (String datum) {
+        int tag = getTag(datum);
+        int monat = getMonat(datum);
+        int jahr = getJahr(datum);
+
+        int [] tageImMonat = new int[] {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        if (istSchaltjahr(jahr)) {
+            tageImMonat[1] = 29;
+        }
+
+        tag++;
+
+        if (tag > tageImMonat[monat - 1]) {
+            tag = 1;
+            monat++;
+            if (monat > 12) {
+                monat = 1;
+                jahr++;
+            }
+        }
+        return zuDatum(tag, monat, jahr);
+    }
+
     public static void main(String[] args) {
         System.out.println("Der Tag:");
         System.out.println(getTag("17.10.2025"));
@@ -73,5 +125,9 @@ public class Kalender {
         System.out.println(getJahr("17.10.2025"));
         System.out.println("\nIst Schaltjahr?:");
         System.out.println(istSchaltjahr(2024));
+        System.out.println("\nDas Datum zusammengebaut:");
+        System.out.println(zuDatum(11, 2,2025));
+        System.out.println("\nNächster Tag:");
+        System.out.println(naechsterTag("30.11.2025"));
     }
 }
