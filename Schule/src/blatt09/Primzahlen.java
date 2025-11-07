477b8f403bbce374c15993eb1ba111db90802dec
LeckerSchmecker123 <lmuth@willigis-online.de>
2025-11-07 15:56:04 +0100

    Blatt 09 fertig (Auf AB noch tabelle ausfüllen)

--- Schule/src/blatt09/Primzahlen.java
+++ Schule/src/blatt09/Primzahlen.java
@@ -1,5 +1,7 @@
 package blatt09;
 
+import java.util.Arrays;
+
 public class Primzahlen {
 
     /**
@@ -20,8 +22,69 @@ public class Primzahlen {
         return true;
     }
 
+    /**
+     * Die Prozedur gibt alle Primzahl bis zu einer angegebenen Grenze aus
+     * @param Grenze Zahl (Obergrenze)
+     */
+    public static void printPrim(int Grenze) {
+        System.out.println("Alle Primzahlen bis " + Grenze + ":");
+
+        for (int i = 2; i <= Grenze; i++) {
+            if (istPrim(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    /**
+     * Die Prozedur zählt die ersten n Primzahlen auf
+     * @param n Anzahl an aufgezählten Primzahlen
+     * @return Ausgabe des Arrays
+     */
+    public static int[] generierePrimzahlen (int n) {
+        int[] primzahlen = new int[n];
+        int zahl = 2;
+        int count = 0;
+
+        while (count < n) {
+            if (istPrim(zahl)) {
+                primzahlen[count] = zahl;
+                count++;
+            }
+            zahl++;
+        }
+
+        return primzahlen;
+    }
+
+    public static void primfaktorzerlegung(int zahl) {
+        System.out.print(zahl + " = ");
+        int teiler = 2;
+
+        while (zahl > 1) {
+            if (zahl % teiler == 0) {
+                System.out.print(teiler);
+                zahl /= teiler;
+                if (zahl > 1) System.out.print(" * ");
+            } else {
+                teiler++;
+            }
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         System.out.println(istPrim(17));
         System.out.println(istPrim(9));
+
+        printPrim(17);
+
+        int[] liste = generierePrimzahlen(1);
+        System.out.println("Erste 10 Primzahlen: ");
+        for (int p : liste) System.out.print(p + " ");
+        System.out.println();
+        System.out.println("Primfaktorzerlegung");
+        primfaktorzerlegung(187);
     }
 }
