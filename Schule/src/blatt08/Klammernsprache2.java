11ce5d0c0c0ae1d7e8a1d91ca64218ac7cf505f8
LeckerSchmecker123 <lmuth@willigis-online.de>
2025-11-25 19:42:06 +0100

    Wiederholung für Klausur

--- Schule/src/blatt08/Klammernsprache2.java
+++ Schule/src/blatt08/Klammernsprache2.java
@@ -1,14 +1,8 @@
 package blatt08;
 
-public class Klammernsprache {
-
-    /**
-     * Die Prozedur überprüft, ob das "Klammerwort" richtig ist
-     * @param str Klammerwort
-     * @return ausgabe als boolean
-     */
-    public static boolean istKlammerwort (String str) {
-        char [] charr = str.toCharArray();
+public class Klammernsprache2 {
+    public static boolean istKlammerwort (String a) {
+        char[] charr = a.toCharArray();
         int value = 0;
         for (int i = 0; i < charr.length; i++) {
             if (value < 0) {
@@ -22,9 +16,7 @@ public class Klammernsprache {
         }
         return value == 0;
     }
-
-
     public static void main(String[] args) {
-        System.out.println(istKlammerwort(")("));
+        System.out.println(istKlammerwort("()"));
     }
 }
