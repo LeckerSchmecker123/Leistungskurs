da7a5d6deccaea37c3a7e77d4ca26a637f304f01
LeckerSchmecker123 <lmuth@willigis-online.de>
2025-12-03 11:19:06 +0100

    Bibliothek

--- Schule/src/blatt14/MultiArrays.java
+++ Schule/src/blatt14/MultiArrays.java
@@ -32,6 +32,14 @@ public class MultiArrays {
         }
     }
 
+    /**
+     * Die Prozedur erstellt einen 2D-Array mit der länge a * b und füllt diesen mit zufälligen Werten, welche zwischen zwei grenzen liegen
+     * @param a 1. Länge des Arrays
+     * @param b 2. Länge des Arrays
+     * @param p untere grenze des zufälligen Wertes
+     * @param q obere grenze des zufälligen Wertes
+     * @return ausgabe des 2D-Arrays
+     */
     public static int[][] createRandom2DIntArrays (int a, int b, int p, int q) {
         int[][] arr = new int[a][b];
         for (int i = 0; i < arr.length; i++) {
@@ -42,6 +50,12 @@ public class MultiArrays {
         return arr;
     }
 
+    /**
+     * Die Prozedur erstellt einen 2D-Array mit der länge a * b und füllt diesen mit zufälligen Werten, welche zwischen 0 und 1 liegen
+     * @param a 1. Länge des Arrays
+     * @param b 2. Länge des Arrays
+     * @return ausgabe des 2D-Arrays
+     */
     public static double[][] createRandom2DDoubleArrays (int a, int b) {
         double[][] arr = new double[a][b];
         for (int i = 0; i < arr.length; i++) {
@@ -50,6 +64,52 @@ public class MultiArrays {
             }
         }
         return arr;
+    }
+
+    /**
+     * Erstellt einen durchnummerierten 2D-Array
+     * @param a 1. Länge des Arrays
+     * @param b 2. Länge des Arrays
+     * @return ausgabe des 2D-Arrays
+     */
+    public static int[][] createCountingArrays (int a, int b) {
+        int[][] arr = new int[a][b];
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = count;
+                count++;
+            }
+        }
+        return arr;
+    }
+
+    /**
+     * Erstellt einen mit Leerzeichen gefüllten 2D-Array
+     * @param a 1. Länge des Arrays
+     * @param b 2. Länge des Arrays
+     * @return ausgabe des 2D-Arrays
+     */
+    public static char[][] createEmpty2DArray (int a, int b) {
+        char[][] arr = new char[a][b];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = ' ';
+            }
+        }
+        return arr;
+    }
+
+    public static boolean istIdentisch (char[][] arr1, char[][] arr2) {
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1[i].length; j++) {
+                if (arr1[i][j] != arr2[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+        //TODO: Noch nicht fertig
     }
 
     public static void main(String[] args) {
@@ -68,5 +128,12 @@ public class MultiArrays {
         System.out.println("Random-Double-Array:");
         double[][] randomCharr = createRandom2DDoubleArrays(5, 5);
         print2DArray(randomCharr);
+        System.out.println("Counting Array: ");
+        int[][] countingArr = createCountingArrays(5, 5);
+        print2DArray(countingArr);
+        System.out.println(countingArr[0][0]);
+        System.out.println("Leerzeichen Array: ");
+        char[][] emptyChar= createEmpty2DArray(10, 5);
+        print2DArray(emptyChar);
     }
 }
