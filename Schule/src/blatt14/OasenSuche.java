23d15b5af75b5ecbea520a8a75e3da10393410b1
LeckerSchmecker123 <lmuth@willigis-online.de>
2026-01-06 22:26:30 +0100

    Blatt 14 weitergehustle

--- Schule/src/blatt14/OasenSuche.java
+++ Schule/src/blatt14/OasenSuche.java
@@ -1,4 +1,23 @@
 package blatt14;
 
+import schisch_visualizer.SchischVisualizer;
+
 public class OasenSuche {
+
+    static char[][] spielfeld;
+
+    public static void initialisiereSpielfeld (int h, int b) {
+        spielfeld = blatt14.MultiArrays.createEmpty2DArray(h, b);
+    }
+
+    public static void zufallsPositionSpieler (char[][] charr) {
+        if (charr != null) {
+            charr[(int) (Math.random() * spielfeld.length)][(int) (Math.random() * spielfeld[0].length)] = 'P';
+        }
+    }
+
+
+    public static void main(String[] args) {
+        SchischVisualizer sv = new SchischVisualizer();
+    }
 }
