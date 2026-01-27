b0005bacdfcd612b4a32fc6327e15c151f204f59
LeckerSchmecker123 <lmuth@willigis-online.de>
2026-01-27 12:59:37 +0100

    blatt 15 angefangen farben weiter gekocht

--- Schule/src/blatt15/KomplexerKaese.java
+++ Schule/src/blatt15/KomplexerKaese.java
@@ -1,4 +1,7 @@
 package blatt15;
 
+import blatt14.Simulationen;
+
 public class KomplexerKaese {
+
 }
---
+++ Schule/src/blatt15/Spiegel.java
@@ -0,0 +1,10 @@
+package blatt15;
+
+public class Spiegel {
+
+    static char[][] feld;
+
+
+    public static void zufallSpiegel () {}
+
+}
