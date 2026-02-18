210bd38ef3e543706fd86672dee9b91264b3b7bf
LeckerSchmecker123 <lmuth@willigis-online.de>
2026-02-18 11:19:55 +0100

    blatt16 woahghhhhhh

---
+++ Schule/src/blatt16/Labyrinth.java
@@ -0,0 +1,28 @@
+package blatt16;
+
+import java.io.File;
+
+public class Labyrinth {
+
+    public char[][] leseLabyrinth (int laby) {
+
+        File f = new File("io/laby" + laby + ".txt");
+        return null;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    public void labyrithSimulationen () {}
+    public static void main(String[] args) {
+
+    }
+}
--- Schule/src/blatt16/Suchkommando.java
+++ Schule/src/blatt16/Suchkommando.java
@@ -6,19 +6,9 @@ import java.io.FileWriter;
 public class Suchkommando {
 
     public static void suchen (String dateiname, String dateiendung) {
-        try {
-            File u = new File("/Users/lmuth");
-            String[] s = u.list();
-            blatt07.ArbeitMitArrays.printArray(s);
-            /*File f = new File("Schule/io/forSearch/" + dateiname + "." + dateiendung);
-            if  (f.exists()) {
-                System.out.println(f.getAbsolutePath());
-            }
-             */
-        }  catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+
     }
+
 
 
     public static void main(String[] args) {
