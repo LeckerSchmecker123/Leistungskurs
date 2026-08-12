4ef29d67d0ba9f748aabf1bc0f84ccf703afdddf
Lenniosaurus <lmuth@willigis-online.de>
2026-08-12 11:05:16 +0200

    Adjazenzmatrix

--- Schule/src/blatt32/aufgabe03/Graph.java
+++ Schule/src/blatt32/aufgabe03/Graph.java
@@ -1,34 +1,110 @@
 package blatt32.aufgabe03;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Graph {
 
-    private int[][] Adjazenzmatrix;
-    private int Knoten;
-    private int[] arr;
+    private int[][] adjazenxmatrix;
 
     public Graph() {
-        this.Adjazenzmatrix = null;
-        this.Knoten = 0;
+        adjazenxmatrix = new int[0][0];
     }
 
-    public Graph(int[][] Adjazenzmatrix,  int Knoten) {
-        this.Adjazenzmatrix = Adjazenzmatrix;
-        this.Knoten = Knoten;
-        this.arr = new int[Knoten];
+    public  Graph (int size) {
+        adjazenxmatrix = new int[size][size];
     }
 
     public void addVertex() {
-        int[][] tempArr = new int[Knoten + 1][Knoten + 1];
-        for (int i = 0; i <= Knoten; i++) {
-            for (int j = 0; j <= Knoten; j++) {
-                try {
-                    tempArr[i][j] = this.Adjazenzmatrix[i][j];
-                } catch (Exception e) {
-                    System.out.println(e);
+        int[][] adj = new int[this.adjazenxmatrix.length + 1][this.adjazenxmatrix[0].length + 1];
+
+        for (int i = 0; i < this.adjazenxmatrix.length; i++) {
+            for (int j = 0; j < this.adjazenxmatrix[0].length; j++) {
+                adj[i][j] = this.adjazenxmatrix[i][j];
+            }
+        }
+
+        this.adjazenxmatrix = adj;
+    }
+
+    public void addEdge(int from, int to, int weight, boolean isDirected) {
+        if (from == to || from < 0 || to < 0 || to >= adjazenxmatrix.length || from >= adjazenxmatrix.length) {
+            throw new IllegalArgumentException();
+        }
+        if (isDirected) {
+            this.adjazenxmatrix[from][to] = weight;
+        } else  {
+            this.adjazenxmatrix[from][to] = weight;
+            this.adjazenxmatrix[to][from] = weight;
+        }
+    }
+
+    public void addEdge(int from, int to, int weight) {
+        addEdge(from, to, weight, false);
+    }
+
+    public void addEdge(int from, int to) {
+        addEdge(from, to, 1, false);
+    }
+
+    public void addEdge(int from, int to, boolean isDirected) {
+        addEdge(from, to, 1, isDirected);
+    }
+
+    public int size() {
+        return this.adjazenxmatrix.length;
+    }
+
+    public boolean isWeighted() {
+        for (int i = 0; i < this.adjazenxmatrix.length; i++) {
+            for (int j = 0; j < this.adjazenxmatrix[0].length; j++) {
+                if (this.adjazenxmatrix[i][j] > 1) {
+                    return true;
                 }
             }
         }
-        this.Adjazenzmatrix = tempArr;
+        return false;
+    }
+
+    public boolean isDirected() {
+        for (int i = 0; i < this.adjazenxmatrix.length; i++) {
+            for (int j = i + 1; j < this.adjazenxmatrix[0].length; j++) {
+                if (this.adjazenxmatrix[i][j] != this.adjazenxmatrix[j][i]) {
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public int[][] getAdjacencyMatrix() {
+        int[][] copiedMatrix = new int[this.adjazenxmatrix.length][this.adjazenxmatrix[0].length];
+        for (int i = 0; i < this.adjazenxmatrix.length; i++) {
+            for (int j = 0; j < this.adjazenxmatrix[0].length; j++) {
+                copiedMatrix[i][j] = this.adjazenxmatrix[i][j];
+            }
+        }
+        return copiedMatrix;
+    }
+
+    public int[] getNeighbours(int knoten) {
+        int[] neighbours = new int[this.adjazenxmatrix.length];
+        for (int i = 0; i < this.adjazenxmatrix.length; i++) {
+            if (this.adjazenxmatrix[i][knoten] != 0) {
+                neighbours[i] = this.adjazenxmatrix[i][knoten];
+            }
+        }
+        return neighbours;
+    }
+
+    public void exportHTML(String filename) {
+        try {
+            File f = new File("Schule/io/export/" + filename + ".html");
+            FileWriter fw = new FileWriter(f);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
 }
---
+++ Schule/src/blatt32/aufgabe03/Main.java
@@ -0,0 +1,8 @@
+package blatt32.aufgabe03;
+
+public class Main {
+    public static void main(String[] args) {
+        Graph graph = new Graph();
+
+    }
+}
